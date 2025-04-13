c09bb886c200f4fade94a7826f2a1c852724c7c4
dipakbhise <dipakbhise420@gmail.com>
2025-04-14 00:08:57 +0530

    feat: PR page

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import PrPage from './components/prPage'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <PrPage/>
+    </>
+  )
+}
+
+export default App
